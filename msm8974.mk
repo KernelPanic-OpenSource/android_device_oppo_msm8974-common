c5f74db9f7ebe2aa53d2040c1d8093e1bd65c263
dopaemon <polarisdp@gmail.com>
2021-01-16 23:18:48 +0700

    Halium
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- msm8974.mk
+++ msm8974.mk
@@ -58,8 +58,8 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    camera.device@1.0-impl \
-    Snap
+    camera.device@1.0-impl
+#     Snap
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
